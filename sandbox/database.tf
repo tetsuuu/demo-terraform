6902e7400516d4031363f17e3a5182d7816e95e3
uluru-kinoshitat <t_kinoshita@uluru.jp>
2019-12-27 17:02:25 +0900

    add database

---
+++ sandbox/database.tf
@@ -0,0 +1,29 @@
+module "aws_db_instance" {
+  source = "../modules/database"
+
+  environment                  = var.environment
+  service_name                 = var.service_name
+  short_env                    = var.short_env
+  maintenance_cidr_blocks      = var.cidr_block
+  identifier                   = "enphoto-prod"
+  storage                      = 20
+  max_storage                  = 100
+  engine                       = "mysql"
+  engine_version               = "5.7.22"
+  publicly_accessible          = true
+  instance_class               = "db.m5.large"
+  name                         = "proxy"
+  user                         = "proxy"
+  password                     = "" // TODO
+  backup_window                = "18:00-18:30"
+  maintenance_window           = "Sun:18:30-Sun:19:00"
+  db_subnet                    = module.service_vpc.public_subnets  //TODO
+  target_vpc                   = module.service_vpc.vpc_id
+  vpc_cidr_block               = var.cidr_block
+  deletion_protection          = false
+  skip_final_snapshot          = true
+  auto_minor_version_upgrade   = false
+  apply_immediately            = true
+  performance_insights_enabled = true
+  developers_site              = var.developers_site
+}
--- sandbox/ec2.tf
+++ sandbox/ec2.tf
@@ -1,13 +1,14 @@
 module "bastion" {
   source = "../modules/ec2"
 
-  common_key    = ""  //TODO
-  instance_type = ""  //TODO
-  environment   = var.environment
-  region        = var.region
-  service_name  = var.service_name
-  short_env     = var.short_env
-  local_ips     = var.local_ips
-  public_sub    = var.public_sub
-  service_vpc   = var.service_vpc
+  common_key      = "sandbox_dev"  //TODO
+  instance_type   = "t2.micro"  //TODO
+  environment     = var.environment
+  region          = var.region
+  service_name    = var.service_name
+  short_env       = var.short_env
+  developers_site = var.developers_site
+  public_subs     = module.service_vpc.public_subnets
+  service_vpc     = module.service_vpc.vpc_id
+  db_sg           = module.aws_db_instance.db_sg
 }
